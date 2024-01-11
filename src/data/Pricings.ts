7a44900a29332d5e476cb179c76ae6f2b1c0c604
Kieran Proctor <kieran.lee.proctor@gmail.com>
2024-01-11 16:43:28 +0000

    refactor: changed pricing text to be same

--- src/data/Pricings.ts
+++ src/data/Pricings.ts
@@ -3,7 +3,6 @@ import type Pricing from '@/interfaces/Pricing';
 const Pricings: Pricing[] = [
   {
     name: 'Base Installer & 2023 Update',
-    href: '/products/base-installer',
     price: '£39.95',
     description: 'Everything necessary to get started plus up-to-date data.',
     features: [
@@ -15,7 +14,6 @@ const Pricings: Pricing[] = [
   },
   {
     name: '2023 Data Package',
-    href: '#',
     price: '£34.95',
     description: 'The latest data package for London Control.',
     features: ['2023 update', 'Updates until 31st December 2024'],
@@ -23,9 +21,8 @@ const Pricings: Pricing[] = [
   },
   {
     name: 'Subsequent Data Package',
-    href: '#',
     price: '£24.95',
-    description: 'The latest data patch for London Control.',
+    description: 'The latest data package for London Control.',
     features: ['XXXX update'],
     isAvailable: false,
   },
