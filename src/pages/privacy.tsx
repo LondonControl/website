844705f7fcf7ceb070a5edffa2862ada38a5f7ce
Kieran Proctor <kieran.lee.proctor@gmail.com>
2024-01-18 14:36:36 +0000

    feat: actual screenshot now added

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -1,12 +1,29 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import type { NextPage } from 'next';
+import { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
 
 import Meta from '@/components/Meta';
 import MainLayout from '@/layouts/Main';
 import { AppConfig } from '@/utils/AppConfig';
 
+// This is a workaround of
+// 'pdfjs-dist/build/pdf.worker.min.js',
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  '../../node_modules/pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url
+).toString();
+
 interface Props {}
 
 const Privacy: NextPage<Props> = () => {
+  const [pageNumber, setPageNumber] = useState(1);
+
+  // eslint-disable-next-line @typescript-eslint/no-shadow
+  function onItemClick({ pageNumber }: { pageNumber: any }) {
+    setPageNumber(pageNumber);
+  }
+
   return (
     <MainLayout
       meta={
@@ -21,7 +38,14 @@ const Privacy: NextPage<Props> = () => {
           Privacy
         </h1>
 
-        <div className="mx-auto mt-6 laptop:mt-12"></div>
+        <div className="mx-auto mt-6 laptop:mt-12">
+          <Document
+            file="/assets/pdfs/lc_privacy_policy.pdf"
+            onItemClick={onItemClick}
+          >
+            <Page pageNumber={pageNumber || 1} />
+          </Document>
+        </div>
       </div>
     </MainLayout>
   );
