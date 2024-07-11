5389e8be0029a937dca4d959961e1f77dda48bbe
KieranLProctor <kieran.lee.proctor@gmail.com>
2024-07-11 11:15:39 +1000

    refactor: moved product to separate card and start redesign

---
+++ src/components/ProductCard/index.tsx
@@ -0,0 +1,72 @@
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+import { useCart } from '@/context/CartContext';
+import { useAuth } from '@/hooks/useAuth';
+import type Product from '@/interfaces/Product';
+
+interface Props {
+  product: Product;
+}
+
+const ProductCard: React.FC<Props> = ({ product }) => {
+  const { user } = useAuth({ middleware: 'guest' });
+  const { addToCart, removeFromCart, cartContainsItem } = useCart();
+
+  return (
+    <div
+      key={product.id}
+      className="group relative flex flex-col overflow-hidden rounded-md border border-gray-200 bg-white"
+    >
+      <Link href={`/products/${product.id}`}>
+        <div className="aspect-h-4 aspect-w-3 bg-gray-200 tablet:aspect-none group-hover:opacity-75 tablet:h-48">
+          <img
+            src={
+              Object.keys(product.images || []).length === 0
+                ? `https://placehold.co/300x300?text=LC`
+                : Object.values(product.images || [])[0]?.original_url
+            }
+            alt={product.title}
+            className="size-full object-cover object-center tablet:size-full"
+          />
+        </div>
+      </Link>
+
+      <div className="flex flex-1 flex-col space-y-2 p-4">
+        <h3 className="text-sm font-medium text-primary">{product.title}</h3>
+
+        <div className="flex flex-1 flex-col justify-end">
+          <p className="text-base font-semibold text-primary">
+            £{product.price / 100}
+          </p>
+        </div>
+
+        {user && (
+          <>
+            {cartContainsItem(product.id) ? (
+              <Button
+                type="button"
+                variant="ghost"
+                className="w-full"
+                onClick={() => removeFromCart(product.id)}
+              >
+                Remove from basket
+              </Button>
+            ) : (
+              <Button
+                type="button"
+                variant="ghost"
+                className="w-full"
+                onClick={() => addToCart(product)}
+              >
+                Add to basket
+              </Button>
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,13 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import type { NextPage } from 'next';
-import Link from 'next/link';
 import MoonLoader from 'react-spinners/MoonLoader';
 import useSWR from 'swr';
 
 import Meta from '@/components/Meta';
-import { Button } from '@/components/ui/button';
-import { useCart } from '@/context/CartContext';
-import { useAuth } from '@/hooks/useAuth';
+import ProductCard from '@/components/ProductCard';
 import type Product from '@/interfaces/Product';
 import MainLayout from '@/layouts/Main';
 import { fetcher } from '@/lib/axios';
@@ -17,12 +14,10 @@ import { getProductsEndpoint } from '@/utils/Endpoints';
 interface Props {}
 
 const Products: NextPage<Props> = () => {
-  const { user } = useAuth({ middleware: 'guest' });
   const { data, error, isLoading } = useSWR(
     getProductsEndpoint('?paginate=none&is_available=1'),
     fetcher,
   );
-  const { addToCart, removeFromCart, cartContainsItem } = useCart();
 
   // eslint-disable-next-line no-console
   if (error) console.log(error);
@@ -49,60 +44,7 @@ const Products: NextPage<Props> = () => {
         ) : (
           <div className="mt-6 grid grid-cols-1 gap-y-4 tablet:grid-cols-2 tablet:gap-x-6 tablet:gap-y-10 laptop:mt-12 laptop:grid-cols-4 laptop:gap-x-8">
             {data?.data.map((product: Product) => (
-              <div
-                key={product.id}
-                className="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white"
-              >
-                <Link href={`/products/${product.id}`}>
-                  <div className="aspect-h-4 aspect-w-3 bg-gray-200 tablet:aspect-none group-hover:opacity-75 tablet:h-48">
-                    <img
-                      src={
-                        Object.keys(product.images || []).length === 0
-                          ? `https://placehold.co/300x300?text=LC`
-                          : Object.values(product.images || [])[0]?.original_url
-                      }
-                      alt={product.title}
-                      className="size-full object-cover object-center tablet:size-full"
-                    />
-                  </div>
-                </Link>
-
-                <div className="flex flex-1 flex-col space-y-2 p-4">
-                  <h3 className="text-sm font-medium text-primary">
-                    {product.title}
-                  </h3>
-
-                  <div className="flex flex-1 flex-col justify-end">
-                    <p className="text-base font-medium text-primary">
-                      £{product.price / 100}
-                    </p>
-                  </div>
-
-                  {user && (
-                    <>
-                      {cartContainsItem(product.id) ? (
-                        <Button
-                          type="button"
-                          variant="ghost"
-                          className="w-full"
-                          onClick={() => removeFromCart(product.id)}
-                        >
-                          Remove from basket
-                        </Button>
-                      ) : (
-                        <Button
-                          type="button"
-                          variant="ghost"
-                          className="w-full"
-                          onClick={() => addToCart(product)}
-                        >
-                          Add to basket
-                        </Button>
-                      )}
-                    </>
-                  )}
-                </div>
-              </div>
+              <ProductCard key={product.id} product={product} />
             ))}
           </div>
         )}
