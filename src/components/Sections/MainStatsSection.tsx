78ba89653acfd4971c6c39949d48d0801ed16480
KieranLProctor <kieran.lee.proctor@gmail.com>
2025-06-03 19:51:53 +1000

    refactor: changes landing page stats

--- src/components/Sections/MainStatsSection.tsx
+++ src/components/Sections/MainStatsSection.tsx
@@ -28,14 +28,14 @@ const MainStatsSection: React.FC<Props> = () => {
               Airport and Approach Control Sectors
             </dt>
             <dd className="order-first text-3xl font-semibold tracking-tight text-white tablet:text-5xl">
-              21
+              25
             </dd>
           </div>
 
           <div className="mx-auto flex max-w-xs flex-col gap-y-4">
             <dt className="text-base leading-7 text-gray-400">Flight plans</dt>
             <dd className="order-first text-3xl font-semibold tracking-tight text-white tablet:text-5xl">
-              19,600+
+              21,000+
             </dd>
           </div>
         </dl>
